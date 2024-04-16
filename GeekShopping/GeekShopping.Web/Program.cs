88e7e6d8110b0f26317593dd6c280b74eec27c2f
Yuri Macedo <yuri_paulino@hotmail.com>
2024-04-15 23:39:37 -0300

    fourth commit

--- GeekShopping/GeekShopping.Web/Program.cs
+++ GeekShopping/GeekShopping.Web/Program.cs
@@ -1,7 +1,11 @@
+using GeekShopping.Web.Service;
+using GeekShopping.Web.Service.IService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<IProductService, ProductService>(c => c.BaseAddress = new Uri(builder.Configuration["ServicesUrls:ProductAPI"]));
 
 var app = builder.Build();
 
